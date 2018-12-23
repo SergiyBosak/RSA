f3d57e2203a40674efa185f514349538e71e425e
Олег <oleg.nazarenko.dev@gmail.com>
2018-12-23 03:41:06 +0200

    Оптимизировал Навел порядок

--- RSA/Program.cs
+++ RSA/Program.cs
@@ -1,37 +1,295 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
-using System.IO;
 using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace RSA
 {
     class Program
     {
-        const string ERROR_LOG = "errors.log";
+        protected const int MAX_ITERATION_COUNT = 10000; // чтобы не зависнуть в случае чего
+        protected const int SHIFT = 2;
+
+        protected static string Alphabet;
 
         static void Main(string[] args)
         {
             while (true)
             {
-                //CompareFunc_simpleNums();
+                if (!GetKeysAndN(out BigInteger publicKey, out BigInteger privateKey, out BigInteger n))
+                {
+                    Console.WriteLine($"Ошибка: не удалось сгенерировать простые числа за { MAX_ITERATION_COUNT } попыток");
+                    Console.WriteLine("Нажмите Escape, чтобы выйти или любую клавишу, чтобы повторить");
 
-                simpleNums(out BigInteger p, out BigInteger q);
+                    continue;
+                }
 
-                BigInteger Fn = (p - 1) * (q - 1);
+                Console.WriteLine("N: " + n);
+                Console.WriteLine("Открытый ключ: " + publicKey);
+                Console.WriteLine("Закрытый ключ: " + privateKey);
+
+                Console.WriteLine();
+
+                string text = EnterText("Введите шифруемый текст:");
+
+                List<BigInteger> result = EncriptRSA(text, publicKey, n);
+
+                Console.WriteLine("Шифрованное сообщение:");
+
+                foreach (var item in result)
+                    Console.WriteLine(item);
+
+                Console.WriteLine();
+                Console.WriteLine("Нажмите любую клавишу, чтобы расшифровать");
+                Console.ReadKey();
+
+                text = DecriptRSA(result, privateKey, n);
+
+                Console.WriteLine(text);
+
+                Console.WriteLine("Нажмите Escape, чтобы выйти или любую клавишу, чтобы повторить");
+
+                if (Console.ReadKey(true).Key == ConsoleKey.Escape)
+                    break;
+
+                Console.WriteLine();
+            }
+        }
+
+        protected static bool GetKeysAndN(out BigInteger publicKey, out BigInteger privateKey, out BigInteger n)
+        {
+            publicKey = -1;
+            privateKey = -1;
+            n = -1;
+
+            if (!GetSimpleNums(out BigInteger p, out BigInteger q))
+                return false;
+
+            BigInteger Fn = (p - 1) * (q - 1);
+            Console.WriteLine("Функция Эйлера: " + Fn);
+
+            publicKey = Exp(Fn);
+            privateKey = ModInverse(publicKey, Fn);
+
+            n = p * q;
+
+            return true;
+        }
+
+        protected static void SetAlphabet(string fromText)
+        {
+            Alphabet = string.Empty;
+
+            foreach (var item in fromText.Distinct())
+                Alphabet += item;
+        }
+
+        protected static bool GetSimpleNums(out BigInteger p, out BigInteger q)
+        {
+            p = -1;
+            q = -1;
+
+            int iterationCount = 0;
+
+            BigInteger min = new BigInteger(1000000000);
+            BigInteger max = new BigInteger(9999999999);
+
+            BigInteger pResult = Environment.TickCount;
+            BigInteger qResult = DateTime.Now.Ticks;
+
+            var watch = Stopwatch.StartNew();
+
+            do
+            {
+                ++iterationCount;
+
+                pResult = GetRandomBigInteger(qResult, min, max, true);
+                qResult = GetRandomBigInteger(pResult, min, max, true);
+            }
+            while (iterationCount < MAX_ITERATION_COUNT && !(IsSimpleNumber(pResult) && IsSimpleNumber(qResult)));
+
+            watch.Stop();
+
+            if (iterationCount == MAX_ITERATION_COUNT)
+                return false;
+
+            p = pResult;
+            q = qResult;
+
+            Console.WriteLine($"Затрачено времени на генерацию простых чисел: { watch.ElapsedMilliseconds } мс");
+            Console.WriteLine($"Потребовалось итераций: { iterationCount }");
+            Console.WriteLine($"P: { pResult }");
+            Console.WriteLine($"Q: { qResult }");
+
+            return true;
+        }
+
+        protected static BigInteger ModInverse(BigInteger a, BigInteger n)
+        {
+            BigInteger i = n, v = 0, d = 1;
+
+            while (a > 0)
+            {
+                BigInteger t = i / a, x = a;
+
+                a = i % x;
+                i = x;
+                x = d;
+                d = v - t * x;
+                v = x;
+            }
+
+            v %= n;
+
+            if (v < 0)
+                v = (v + n) % n;
+
+            return v;
+        }
+
+        public static List<BigInteger> EncriptRSA(string text, BigInteger e, BigInteger n)
+        {
+            SetAlphabet(text);
+
+            List<BigInteger> result = new List<BigInteger>();
+
+            BigInteger index = 0;
+
+            foreach (var letter in text)
+            {
+                index = (Alphabet.IndexOf(letter) + index + SHIFT) % n;
+
+                result.Add(BigInteger.ModPow(index, e, n));
+            }
+
+            return result;
+        }
+
+        public static string DecriptRSA(List<BigInteger> input, BigInteger d, BigInteger n)
+        {
+            string result = string.Empty;
+
+            BigInteger currentValue = 0;
+            BigInteger previouslyValue = 0;
+
+            foreach (var item in input)
+            {
+                currentValue = BigInteger.ModPow(item, d, n);
+
+                result += Alphabet[(int)(currentValue - previouslyValue) - SHIFT];
+
+                previouslyValue = currentValue;
+            }
+
+            return result;
+        }
+
+        protected static BigInteger Exp(BigInteger Fn)
+        {
+            BigInteger result;
+
+            do
+                result = GetRandomBigInteger(DateTime.Now.Millisecond, 10, Fn, true);
+            while (BigInteger.GreatestCommonDivisor(result, Fn) != 1);
+
+            return result;
+        }
+
+        protected static BigInteger GetRandomBigInteger(BigInteger seed, BigInteger minValue, BigInteger maxValue, bool checkThatNotIsEven = false)
+        {
+            if (maxValue < minValue || maxValue == 0)
+                return -1;
+
+            byte[] bytes = maxValue.ToByteArray();
+
+            BigInteger result;
+
+            var random = new Random(GetInt32FromBigInteger(seed));
+
+            do
+            {
+                random.NextBytes(bytes);
+
+                bytes[bytes.Length - 1] &= 0x7F; // принудительно делаем положительным числом
+
+                result = new BigInteger(bytes);
+            }
+            while (
+                (checkThatNotIsEven && result.IsEven)
+                || result < minValue
+                || result > maxValue
+            );
+
+            return result;
+        }
+
+        protected static int GetInt32FromBigInteger(BigInteger value)
+        {
+            var valueBytes = value.ToByteArray();
+
+            int result = 0;
+            int length = Math.Min(valueBytes.Length, 4);
+
+            for (int i = 0; i < length; i++)
+                result += valueBytes[i] << (i * 8);
+
+            return result;
+        }
+
+        protected static bool IsSimpleNumber(BigInteger number)
+        {
+            if (number.IsEven)
+                return false;
+
+            BigInteger d = 3;
+            
+            while (d * d <= number && number % d != 0)
+                d += 2;
+
+            return d * d > number;
+        }
+
+        protected static string EnterText(string message)
+        {
+            string result = null;
+
+            do
+                Console.WriteLine(message);
+            while ((result = Console.ReadLine().Trim()) == string.Empty);
+
+            return result;
+        }
+
+        protected static int EnterNumber(string message)
+        {
+            int num = 0;
+
+            do
+                Console.Write(message);
+            while (!int.TryParse(Console.ReadLine(), out num));
+
+            return num;
+        }
+
+/*
+        static void Main_old(string[] args)
+        {
+            while (true)
+            {
+                int Fn = eilerFunc();
 
                 Console.WriteLine("Функция Эйлера: " + Fn);
 
-                BigInteger n = p * q;
+                int n = ModN();
 
                 Console.WriteLine("n: " + n);
 
-                BigInteger e = exp(Fn);
+                int e = exp(Fn);
 
                 Console.WriteLine("Открытый ключ: " + e);
 
-                BigInteger d = modInverse(e, Fn);
+                int d = DKey(e, Fn);
 
                 Console.WriteLine("Закрытый ключ: " + d);
 
@@ -58,207 +316,31 @@ namespace RSA
 
                 Console.WriteLine(text);
 
-                if (Console.ReadKey(true).Key == ConsoleKey.Escape)
+                if (Console.ReadKey().Key == ConsoleKey.Escape)
                     break;
             }
         }
 
-        static List<BigInteger> EncriptRSA(string alphabet, string text, BigInteger e, BigInteger n)
+
+
+        static int DKey(int e, int Fn)
         {
-            List<BigInteger> result = new List<BigInteger>();
-
-            BigInteger index, indexResult;
-
-            BigInteger a = 0;
-
-            for (int i = 0; i < text.Length; i++)
-            {
-                indexResult = alphabet.IndexOf(text[i]) + 2;
-
-                index = (indexResult + a) % n;
-
-                a = index;
-
-                result.Add(BigInteger.ModPow(index, e, n));
-            }                
- 
-            return result;
+            return modInverse(e, Fn);
         }
 
-        //static void Main_old(string[] args)
-        //{
-        //    while (true)
-        //    {
-        //        int Fn = eilerFunc();
-
-        //        Console.WriteLine("Функция Эйлера: " + Fn);
-
-        //        int n = ModN();
-
-        //        Console.WriteLine("n: " + n);
-
-        //        int e = exp(Fn);
-
-        //        Console.WriteLine("Открытый ключ: " + e);
-
-        //        int d = DKey(e, Fn);
-
-        //        Console.WriteLine("Закрытый ключ: " + d);
-
-        //        string text = enterText("введите шифруемый текст:");
-
-        //        string alphabet = string.Empty;
-
-        //        foreach (var item in text.Distinct())
-        //            alphabet += item;
-
-        //        List<BigInteger> result = EncriptRSA(alphabet, text, e, n);
-
-        //        foreach (var item in result)
-        //            Console.WriteLine(item);
-
-        //        Console.WriteLine("Шифрованное сообщение");
-
-        //        text = result.ToString();
-
-        //        Console.WriteLine("Нажмите любую клавишу, чтобы расшифровать");
-        //        Console.ReadKey();
-
-        //        text = DecriptRSA(alphabet, result, d, n);
-
-        //        Console.WriteLine(text);
-
-        //        if (Console.ReadKey().Key == ConsoleKey.Escape)
-        //            break;
-        //    }
-        //}
-
-        static string DecriptRSA(string alphabet, List<BigInteger> input, BigInteger d, BigInteger n)
+        static int exp_old(int Fn)
         {
-            string result = string.Empty;
-
-            int i = 0;
-
-            BigInteger[] Mas = new BigInteger[input.Count];
-
-            try
-            {
-                BigInteger index;
-
-                BigInteger[] indexMas = new BigInteger[input.Count];
-
-                for (i = 0; i < input.Count; i++)
-                {
-                    index = input[i];
-
-                    index = BigInteger.ModPow(index, d, n);
-
-                    indexMas[i] = indexMas[i] + index;
-                }
-
-                for (i = 0; i < input.Count; i++)
-                {
-                    if (i == 0)
-                        Mas[i] = indexMas[i];
-                    else
-                        Mas[i] = indexMas[i] - indexMas[i - 1];
-
-                    result += alphabet[(int)(Mas[i] - 2)];
-                }
-            }
-            catch (Exception ex)
-            {
-                if (!File.Exists(ERROR_LOG))
-                    using (var file = File.Create(ERROR_LOG))
-                    {
-
-                    }
-
-                using (var log = new StreamWriter(ERROR_LOG, true))
-                {
-                    log.WriteLine("alphabet: " + alphabet);
-                    log.WriteLine("alphabet length: " + alphabet.Length);
-                    log.WriteLine("d: " + d);
-                    log.WriteLine("n: " + n);
-                    log.WriteLine("input[i]: " + input[i]);
-                    log.WriteLine("Mas[i]: " + Mas[i]);
-                }
-
-                Console.WriteLine(ex.Message);
-            }
-
-            return result;
-        }
-        
-        //static int DKey(int e, int Fn)
-        //{
-        //    return modInverse(e, Fn);
-        //}
-
-        static BigInteger exp(BigInteger Fn)
-        {
-            BigInteger result;
+            BigInteger e;
 
             do
             {
                 Random rdm = new Random();
-                
-                result = GetRandomBigInteger(10, Fn, Environment.TickCount);
+
+                e = rdm.Next(10, Fn);
             }
-            while (NOD(result, Fn) != 1);
+            while (NOD(e, Fn) != 1);
 
-            return result;
-        }
-
-        //static int exp_old(int Fn)
-        //{
-        //    BigInteger e;
-
-        //    do
-        //    {
-        //        Random rdm = new Random();
-
-        //        e = rdm.Next(10, Fn);
-        //    }
-        //    while (NOD(e, Fn) != 1);
-
-        //    return e;
-        //}
-
-        static int GetInt32FromBigInteger(BigInteger value)
-        {
-            var valueBytes = value.ToByteArray();
-
-            int result = 0;
-            int length = Math.Min(valueBytes.Length, 4);
-
-            for (int i = 0; i < length; i++)
-                result += valueBytes[i] << (i * 8);
-
-            return result;
-        }
-
-        public static BigInteger GetRandomBigInteger(BigInteger minValue, BigInteger maxValue, BigInteger seed, bool check2Division = false)
-        {
-            if (maxValue < minValue || maxValue == 0)
-                return -1;
-
-            byte[] bytes = maxValue.ToByteArray();
-
-            BigInteger result;
-
-            var random = new Random(GetInt32FromBigInteger(seed));
-
-            do
-            {
-                random.NextBytes(bytes);
-                bytes[bytes.Length - 1] &= (byte)0x7F; // force sign bit to positive
-
-                result = new BigInteger(bytes);
-            }
-            while ((check2Division && result % 2 == 0) || result < minValue || result > maxValue);
-
-            return result;
+            return e;
         }
 
         static BigInteger eilerFunc()
@@ -284,33 +366,6 @@ namespace RSA
             return result;
         }
 
-        static void simpleNums(out BigInteger p, out BigInteger q)
-        {
-            int iterationCount = 0;
-
-            BigInteger pResult = Environment.TickCount;
-            BigInteger qResult = 0;
-
-            var watch = Stopwatch.StartNew();
-
-            do
-            {
-                ++iterationCount;
-
-                pResult = GetRandomBigInteger(1000000000, 9999999999, pResult, true);
-                qResult = GetRandomBigInteger(1000000000, 9999999999, pResult, true);
-            }
-            while (!(simpleNum1(pResult) && simpleNum1(qResult)));
-
-            watch.Stop();
-
-            Console.WriteLine($"Затрачено времени на генерацию простых чисел: { watch.ElapsedMilliseconds } мс");
-            Console.WriteLine($"Потребовалось итераций: { iterationCount }");
-
-            p = pResult;
-            q = qResult;
-        }
-
         static void CompareFunc_simpleNums()
         {
             var watch = Stopwatch.StartNew();
@@ -319,7 +374,7 @@ namespace RSA
             Console.WriteLine($"Затрачено времени на выполнения метода simpleNums_old: { watch.ElapsedTicks }{ Environment.NewLine }P = { p_old }; Q = { q_old }");
 
             watch = Stopwatch.StartNew();
-            simpleNums(out BigInteger p, out BigInteger q);
+            GetSimpleNums(out BigInteger p, out BigInteger q);
             watch.Stop();
             Console.WriteLine($"Затрачено времени на выполнения метода simpleNums: { watch.ElapsedTicks }{ Environment.NewLine }P = { p }; Q = { q }");
         }
@@ -337,7 +392,7 @@ namespace RSA
                 p = rdm.Next(1000, 10000);
                 q = rdm.Next(1000, 10000);
             }
-            while (!(simpleNum1(p) && simpleNum1(q)));
+            while (!(IsSimpleNumber(p) && IsSimpleNumber(q)));
 
             Console.WriteLine($"Потребовалось итераций: { iterationCount }");
         }
@@ -363,93 +418,35 @@ namespace RSA
             return true;
         }
 
-        static bool simpleNum1(BigInteger n)
+        static bool simpleNum1_old(long n)
         {
             if (n % 2 == 0)
                 return false;
 
-            BigInteger d = 3;
-            
+            int d = 3;
+
             while (d * d <= n && n % d != 0)
                 d += 2;
 
             return d * d > n;
         }
 
-        //static bool simpleNum1_old(long n)
-        //{
-        //    if (n % 2 == 0)
-        //        return false;
-
-        //    int d = 3;
-
-        //    while (d * d <= n && n % d != 0)
-        //        d += 2;
-
-        //    return d * d > n;
-        //}
-
-        static BigInteger modInverse(BigInteger a, BigInteger n)
+        static int modInverse_old(int a, int n)
         {
-            BigInteger i = n, v = 0, d = 1;
-
+            int i = n, v = 0, d = 1;
             while (a > 0)
             {
-                BigInteger t = i / a, x = a;
-
+                int t = i / a, x = a;
                 a = i % x;
                 i = x;
                 x = d;
                 d = v - t * x;
                 v = x;
             }
-
             v %= n;
-
-            if (v < 0)
-                v = (v + n) % n;
-
+            if (v < 0) v = (v + n) % n;
             return v;
         }
-
-        //static int modInverse_old(int a, int n)
-        //{
-        //    int i = n, v = 0, d = 1;
-        //    while (a > 0)
-        //    {
-        //        int t = i / a, x = a;
-        //        a = i % x;
-        //        i = x;
-        //        x = d;
-        //        d = v - t * x;
-        //        v = x;
-        //    }
-        //    v %= n;
-        //    if (v < 0) v = (v + n) % n;
-        //    return v;
-        //}
-
-        static string enterText(string text)
-        {
-            string result = null;
-
-            do
-                Console.WriteLine(text);
-            while ((result = Console.ReadLine().Trim()) == string.Empty);
-
-            return result;
-        }
-
-        static int enterNumber(string text)
-        {
-            int num = 0;
-
-            do
-                Console.Write(text);
-            while (!int.TryParse(Console.ReadLine(), out num));
-
-            return num;
-        }
-
+*/
     }
 }
